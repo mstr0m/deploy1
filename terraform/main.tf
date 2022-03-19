671447075793c2ab775f16828427d75549380b24
Mike <mikestromm@gmail.com>
2022-03-19 13:55:14 +0000

    apt broken while adding docker repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,5 +88,8 @@ resource "aws_instance" "k8s-worker" {
 }
 
 output "k8s-master_public_ip" {
-  value = aws_instance.k8s-master.public_ip
+  value = aws_instance.k8s-master.*.public_ip
+}
+output "k8s-worker_public_ip" {
+  value = aws_instance.k8s-worker.*.public_ip
 }
